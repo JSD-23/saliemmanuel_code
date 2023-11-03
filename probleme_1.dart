1ebdfea59c29c93442c9ec3f5c105d2df0865abe
saliemmanuel <saliemmanuel3304@gmail.com>
2023-11-03 08:55:47 +0100

    probleme 1 complete

---
+++ probleme_1.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Problème 1 : La longueur du dernier mot
+  int sizeLastWord({required String mot}) {
+    // cas ou le mot est vide
+    if (mot.isEmpty) {
+      return 0;
+    } else {
+      String dernierMot = mot.split(' ').last;
+      // Cas où le dernier charactère elle même
+      // n'est pas le symbole espace
+      if (dernierMot.isEmpty) {
+        return 0;
+      } else {
+        return dernierMot.length;
+      }
+    }
+  }
+
+  int taille = sizeLastWord(mot: "SALI EMMANUEL");
+  print(taille);
+}
