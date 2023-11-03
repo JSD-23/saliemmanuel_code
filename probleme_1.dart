c31529b34c3ce87cf547e9d43f58deea3d32132b
saliemmanuel <saliemmanuel3304@gmail.com>
2023-11-03 09:58:12 +0100

    problème 2

--- probleme_1.dart
+++ probleme_1.dart
@@ -1,13 +1,13 @@
 void main() {
-  // Problème 1 : La longueur du dernier mot
+  /// Problème 1 : La longueur du dernier mot
   int sizeLastWord({required String mot}) {
-    // cas ou le mot est vide
+    /// cas ou le mot est vide
     if (mot.isEmpty) {
       return 0;
     } else {
       String dernierMot = mot.split(' ').last;
-      // Cas où le dernier charactère elle même
-      // n'est pas le symbole espace
+      /// Cas où le dernier charactère elle même
+      /// n'est pas le symbole espace
       if (dernierMot.isEmpty) {
         return 0;
       } else {
@@ -15,7 +15,4 @@ void main() {
       }
     }
   }
-
-  int taille = sizeLastWord(mot: "SALI EMMANUEL");
-  print(taille);
 }
--- probleme_2.dart
+++ probleme_2.dart
@@ -1,3 +1,28 @@
-void main(){
-  
+void main() {
+  /// Problème 2 : Le meuilleur moment pour vendre
+  ///
+  /// un tableau qui recapitule le prix d'une action
+  /// prix[i] est le prix de l'action
+  /// i est le jour
+  /// on
+  int maxProfit({List<int>? prix, int? size}) {
+    /// verification de la taille de la liste
+    /// avec la taille `[size]`
+    if (prix!.length != size) {
+      return 0;
+    } else {
+      /// verification si tous les prix sont positif ou nuls
+      for (int loop = 0; loop < prix.length; loop++) {
+        if (prix[loop] < 0) {
+          return 0;
+        } else if (prix[loop] == 0) {
+          return 0;
+        }
+      }
+    }
+    return 0;
+  }
+
+  int result = maxProfit(prix: [0, 1, 5, 3, 6, 4], size: 6);
+  print(result);
 }
