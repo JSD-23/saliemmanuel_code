7c9b23e861cf407f5a11fe11ee329d80eb297139
saliemmanuel <saliemmanuel3304@gmail.com>
2023-11-03 10:09:47 +0100

    probleme 3

--- probleme_3.dart
+++ probleme_3.dart
@@ -1,3 +1,8 @@
-void main(){
-  
+void main() {
+  /// Problème 3 : Implementation des caches LRU
+  ///
+  /// fonction LRUInit
+  LRUInit({int? capacite}) {
+    List<Map<String, int>>? caches;
+  }
 }
